45b450daa7a63cf6c31773c112562e7f366fd090
Sam White <samuelwhite98@outlook.com>
2023-03-24 22:37:17 +0000

    Create gameboard display

---
+++ script.js
@@ -0,0 +1,40 @@
+const gameBoard = (() => {
+
+    let boardArray = [
+
+        ["w", "b", "w", "b", "w", "b", "w", "b"],
+        ["b", "w", "b", "w", "b", "w", "b", "w"],
+        ["w", "b", "w", "b", "w", "b", "w", "b"],
+        ["b", "w", "b", "w", "b", "w", "b", "w"],
+        ["w", "b", "w", "b", "w", "b", "w", "b"],
+        ["b", "w", "b", "w", "b", "w", "b", "w"],
+        ["w", "b", "w", "b", "w", "b", "w", "b"],
+        ["b", "w", "b", "w", "b", "w", "b", "w"],
+
+    ];
+
+    return {boardArray}
+})();
+
+const displayController = (() => {
+
+
+    const displayBoard = () => {
+
+        let gameContainer = document.querySelector(".gameboard")
+
+        for(x=0; x<8; x++){
+            for(y=0; y<8; y++){
+                let divClass = gameBoard.boardArray[x][y]
+                let div = document.createElement("div")
+                div.classList.add(`${divClass}`)
+                gameContainer.appendChild(div)
+            }
+        }
+    }
+
+    return{displayBoard}
+
+})()
+
+displayController.displayBoard()
