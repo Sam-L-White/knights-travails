cd295c690c153988f7d16ae5853bccaf39de44d3
Sam White <samuelwhite98@outlook.com>
2023-03-25 21:04:58 +0000

    Add map of all possible moves from squares

--- script.js
+++ script.js
@@ -51,9 +51,12 @@ const eventController = (() => {
             square.addEventListener("click", (e) => {
 
                 e.target.textContent = "♞"
+
+                //Remove event listeners
                 let oldBoard = document.querySelector(".gameboard")
                 let newBoard = oldBoard.cloneNode(true);
                 oldBoard.parentNode.replaceChild(newBoard, oldBoard);
+
                 knightSquare = e.target.id
                 addTargetEvents()
             })
@@ -61,16 +64,19 @@ const eventController = (() => {
     }
 
     const addTargetEvents = () => {
+
         let boardSquares = document.querySelectorAll(".gameboard div")
         boardSquares.forEach(square => {
 
             square.addEventListener("click", (e) => {
 
                 e.target.textContent = "T"
+
                 //Remove event listeners
                 let oldBoard = document.querySelector(".gameboard")
                 let newBoard = oldBoard.cloneNode(true);
                 oldBoard.parentNode.replaceChild(newBoard, oldBoard);
+
                 targetSquare = e.target.id
                 gameController.gameStart(knightSquare, targetSquare)
                 
@@ -85,10 +91,53 @@ const gameController = (() => {
     displayController.displayBoard()
     eventController.addKnightEvents()
 
+    let boardMap = new Map()
+
+    let allMoves = [
+        [1, -2],
+        [2, -1],
+        [2, 1],
+        [1, 2],
+        [-1, 2],
+        [-2, 1],
+        [-2, -1],
+        [-1, -2]
+    ]
+
+    let possibleLocations = []
+
     const gameStart = (knightSquare, targetSquare) => {
-        console.log(knightSquare)
-        console.log(targetSquare)
+        let pathArray = knightMoves(knightSquare, targetSquare)
+        console.log(allMoves)
+        createMap()
     }
+
+    const knightMoves = (knightSquare, targetSquare) => {
+
+    }
+
+    const createMap = () => {
+        for(x = 0; x < 8; x++){
+            for(y = 0; y < 8; y++){
+                allMoves.forEach(move => {
+                    let xLocation = x + move[0]
+                    let yLocation = y + move[1]
+                    if(xLocation >= 0 && yLocation >= 0){
+                        if(xLocation <= 7 && yLocation <= 7){
+                            possibleLocations.push([xLocation, yLocation])
+                        }
+                    }
+                })
+
+                boardMap.set(`${[x,y]}`, possibleLocations)
+                possibleLocations = []
+            }
+        }
+        console.log(boardMap)
+    }
+
+
+
     return{gameStart}
 })()
 
