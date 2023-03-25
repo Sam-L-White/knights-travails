5e9590033e62a0605b4696759e69456d8c7aaa42
Sam White <samuelwhite98@outlook.com>
2023-03-25 15:06:37 +0000

    Add knight/target selector and pass ids to game

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const displayController = (() => {
                 let divClass = gameBoard.boardArray[x][y]
                 let div = document.createElement("div")
                 div.classList.add(`${divClass}`)
-                div.id = `${x},${y}`
+                div.id = `${y},${x}`
                 gameContainer.appendChild(div)
             }
         }
@@ -38,4 +38,57 @@ const displayController = (() => {
 
 })()
 
-displayController.displayBoard()
+const eventController = (() => {
+
+    let knightSquare
+    let targetSquare
+
+    const addKnightEvents = () => {
+
+        let boardSquares = document.querySelectorAll(".gameboard div")
+        boardSquares.forEach(square => {
+
+            square.addEventListener("click", (e) => {
+
+                e.target.textContent = "♞"
+                let oldBoard = document.querySelector(".gameboard")
+                let newBoard = oldBoard.cloneNode(true);
+                oldBoard.parentNode.replaceChild(newBoard, oldBoard);
+                knightSquare = e.target.id
+                addTargetEvents()
+            })
+        })
+    }
+
+    const addTargetEvents = () => {
+        let boardSquares = document.querySelectorAll(".gameboard div")
+        boardSquares.forEach(square => {
+
+            square.addEventListener("click", (e) => {
+
+                e.target.textContent = "T"
+                //Remove event listeners
+                let oldBoard = document.querySelector(".gameboard")
+                let newBoard = oldBoard.cloneNode(true);
+                oldBoard.parentNode.replaceChild(newBoard, oldBoard);
+                targetSquare = e.target.id
+                gameController.gameStart(knightSquare, targetSquare)
+                
+            })
+        })
+    }
+
+    return{addKnightEvents, knightSquare}
+})()
+
+const gameController = (() => {
+    displayController.displayBoard()
+    eventController.addKnightEvents()
+
+    const gameStart = (knightSquare, targetSquare) => {
+        console.log(knightSquare)
+        console.log(targetSquare)
+    }
+    return{gameStart}
+})()
+
